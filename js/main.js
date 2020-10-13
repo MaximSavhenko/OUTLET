44ff6e1fd9fc7717e965d68a57d01bf06d23c42a
maximsavhenko <uspmsm@gmail.com>
2020-10-13 16:28:41 +0300

    fix search input

--- js/main.js
+++ js/main.js
@@ -9,11 +9,14 @@
 
 
 function toggleSearc() {
-    var searchIcon = document.querySelector('.btn__search-icon'),
-        searchInput = document.querySelector('.mini_search_block');
+    var searchIcon = document.querySelector('.navbar-search-toggle'),
+        searchInput = document.querySelector('.mini_search_block'),
+        toogleIcon = document.querySelector('.navbar-search-toggle');
 	searchIcon.onclick = function () {
         searchInput.classList.toggle('active');
+        toogleIcon.classList.toggle('active');
     };
 };
 
 toggleSearc();
+
